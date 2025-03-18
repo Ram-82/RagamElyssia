71475ec24cc34802821ce08b6e8af1ccd834ef70
Ramachandragowda S Patil <121176737+Ram-82@users.noreply.github.com>
2025-03-18 22:30:56 +0530

    Update vite.ts

--- backend/src/vite.ts
+++ backend/src/vite.ts
@@ -2,8 +2,7 @@ import express, { type Express } from "express";
 import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import { createServer as createViteServer } from "vite"; // Import createServer
-import { createLogger } from "vite"; // Import createLogger
+import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
 import { nanoid } from "nanoid";
 
